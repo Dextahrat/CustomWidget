a84bd43299b759e1861f198a468bd47eb2c235dc
dextahrat <dextahrat@gmail.com>
2021-12-13 03:45:54 +0300

    FirstRun

--- CustomWidget/Form1.cs
+++ CustomWidget/Form1.cs
@@ -1,10 +1,54 @@
+using System.Runtime.InteropServices;
+
+
 namespace CustomWidget
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hP, IntPtr hC, string sC, string sW);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetShellWindow();
+
+        private IntPtr GetWindowsHandle()
+        {
+            IntPtr _progmanHndl = GetShellWindow();
+            IntPtr nWinHandle = FindWindowEx(_progmanHndl, IntPtr.Zero, "SHELLDLL_DefView", null);
+
+            if (nWinHandle != IntPtr.Zero) return nWinHandle;
+
+            _progmanHndl = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "Progman", null);
+            nWinHandle = FindWindowEx(_progmanHndl, IntPtr.Zero, "SHELLDLL_DefView", null);
+
+            if (nWinHandle != IntPtr.Zero) return nWinHandle;
+
+            _progmanHndl = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "WorkerW", null);
+            nWinHandle = FindWindowEx(_progmanHndl, IntPtr.Zero, "SHELLDLL_DefView", null);
+
+            return nWinHandle;
+
+        }
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        void MakeWin()
+        {
+            IntPtr a = GetWindowsHandle();
+
+            SetParent(Handle, a);
+        }
+
+
         public Form1()
         {
+            MakeWin();  
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
